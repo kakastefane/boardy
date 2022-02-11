8b28fe017f944c9579205e5e9a627d3843c56e31
kakastefane <karistonstefane@hotmail.com>
2022-02-11 14:25:54 -0300

    created a home page

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export default function Board() {
+  return (
+    <h1>Board</h1>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Boardy</title>
+        <title>Boardy - Organizando suas tarefas!</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Boardy" />
 
-      <main>
-        <h1 className={styles.title}>Index</h1>
+        <section className={styles.callToAction}>
+          <h1>Uma ferramenta para o seu dia a dia. Escreva, planeje e se organize.</h1>
+          <p><span>100% gratuita</span> e online.</p>
+        </section>
+
+        <div className={styles.donaters}>
+          <h3>Apoiadores</h3>
+          <ul>
+            <li><img src="https://avatars.githubusercontent.com/u/8293795?s=120&v=4" alt="Kariston" /></li>
+          </ul>
+        </div>
       </main>
     </>
   )
